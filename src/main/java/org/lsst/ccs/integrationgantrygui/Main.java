f8018a086b16d3122bab4f76793b704b60455a39
Tony Johnson <tonyj@stanford.edu>
2018-05-08 17:12:13 -0700

    Perform initial scan of files at startup (does not currently select the most recently used version). Remove cameraMap which is not currently used.

--- src/main/java/org/lsst/ccs/integrationgantrygui/Main.java
+++ src/main/java/org/lsst/ccs/integrationgantrygui/Main.java
@@ -1,6 +1,5 @@
 package org.lsst.ccs.integrationgantrygui;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.BufferUnderflowException;
 import java.nio.file.Files;
@@ -20,6 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import javax.swing.SwingUtilities;
 import nom.tam.fits.TruncatedFileException;
 import javax.script.ScriptException;
@@ -34,27 +34,30 @@ public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getName());
     private final Path watchDir = Paths.get(System.getProperty("watchDir", "/mnt/ramdisk"));
     private final Pattern watchPattern = Pattern.compile(System.getProperty("watchPattern", ".+(\\d)_rng\\d+.*"));
-    private final Map<Integer, Integer> cameraMap = new HashMap<>();
-    private int i = 0;
-    private int j = 0;
+//    private final Map<Integer, Integer> cameraMap = new HashMap<>();
+    private int totalFiles = 0;
+    private int processFiles = 0;
     private final AtomicInteger count = new AtomicInteger();
+    private JSONParser parser;
+    private IntegrationGantryFrame frame;
+    private LinkedBlockingQueue[] queues;
 
     Main() {
-        cameraMap.put(0, 0);
-        cameraMap.put(1, 1);
-        cameraMap.put(2, 2);
-        cameraMap.put(3, 3);
+//        cameraMap.put(0, 0);
+//        cameraMap.put(1, 1);
+//        cameraMap.put(2, 2);
+//        cameraMap.put(3, 3);
     }
 
     private void start() throws IOException, InterruptedException {
         ExecutorService workQueue = Executors.newCachedThreadPool();
-        IntegrationGantryFrame frame = new IntegrationGantryFrame();
+        frame = new IntegrationGantryFrame();
 
         java.awt.EventQueue.invokeLater(() -> {
             frame.setVisible(true);
         });
 
-        LinkedBlockingQueue[] queues = new LinkedBlockingQueue[4];
+        queues = new LinkedBlockingQueue[4];
         for (int i = 0; i < queues.length; i++) {
             LinkedBlockingQueue<Path> queue = new LinkedBlockingQueue<>(1);
             int index = i;
@@ -86,7 +89,25 @@ public class Main {
             }
         };
         workQueue.execute(runnable);
-        JSONParser parser = new JSONParser();
+        parser = new JSONParser();
+
+        Stream<Path> pathList = Files.list(watchDir);
+        pathList.forEach((path) -> {
+            Path fullPath = watchDir.resolve(path);
+            String fileName = fullPath.getFileName().toString();
+            Matcher matcher = watchPattern.matcher(fileName);
+            if (matcher.matches()) {
+                // TODO: Only handle most recent file for each index
+                int index = Integer.parseInt(matcher.group(1));
+                if (fileName.endsWith(".fits")) {
+                    handleFitsFile(fullPath, index, path);
+                } else if (fileName.endsWith(".txt")) {
+                    handleTextFile(fullPath, index);
+                }
+            } else if (fileName.endsWith(".json")) {
+                handleJSONFile(fileName, fullPath);
+            }
+        });
 
         try (WatchService watchService = watchDir.getFileSystem().newWatchService()) {
             watchDir.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
@@ -99,50 +120,62 @@ public class Main {
                     if (matcher.matches()) {
                         int index = Integer.parseInt(matcher.group(1));
                         if (fileName.endsWith(".fits")) {
-                            if (fullPath.toFile().length() == 5_071_680 || fullPath.toFile().length() == 10_137_600) {
-
-                                LinkedBlockingQueue<Path> queue = queues[index];
-                                Path poll = queue.poll(); // Discard any files not yet processed
-                                if (poll == null) {
-                                    j++;
-                                }
-                                queue.add(watchDir.resolve(path));
-                                i++;
-                                if (i % 100 == 0) {
-                                    LOG.log(Level.FINE, "{0}% of files were processed\n", j);
-                                    j = 0;
-                                }
-                            }
+                            handleFitsFile(fullPath, index, path);
                         } else if (fileName.endsWith(".txt")) {
-                            try {
-                                List<String> text = Files.readAllLines(fullPath);
-                                if (!text.isEmpty()) {
-                                    int findex = index;
-                                    SwingUtilities.invokeLater(() -> {
-                                        frame.setLabel(findex, text.get(0));
-                                    });
-                                }
-                            } catch (IOException ex) {
-                                LOG.log(Level.SEVERE, "Error reading text file", ex);
-                            }
+                            handleTextFile(fullPath, index);
                         }
                     } else if (fileName.endsWith(".json")) {
-                        boolean isHorizontal = fileName.contains("horiz");
-                        try {
-                            Map<String, List<Integer>> rois = parser.parseROI(Files.readAllLines(fullPath));
-                            rois.forEach((t, u) -> {
-                                int index = Integer.parseInt(t);
-                                SwingUtilities.invokeLater(() -> {
-                                    frame.setROI(isHorizontal,index,u);
-                                });
-                            });
-                        } catch (IOException | ScriptException ex) {
-                            LOG.log(Level.SEVERE, "Error reading json file", ex);
-                        }
+                        handleJSONFile(fileName, fullPath);
                     }
                 });
                 take.reset();
             }
+        }
+    }
+
+    private void handleFitsFile(Path fullPath, int index, Path path) {
+        if (fullPath.toFile().length() == 5_071_680 || fullPath.toFile().length() == 10_137_600) {
+
+            LinkedBlockingQueue<Path> queue = queues[index];
+            Path poll = queue.poll(); // Discard any files not yet processed
+            if (poll == null) {
+                processFiles++;
+            }
+            queue.add(watchDir.resolve(path));
+            totalFiles++;
+            if (totalFiles % 100 == 0) {
+                LOG.log(Level.FINE, "{0}% of files were processed\n", processFiles);
+                processFiles = 0;
+            }
+        }
+    }
+
+    private void handleTextFile(Path fullPath, int index) {
+        try {
+            List<String> text = Files.readAllLines(fullPath);
+            if (!text.isEmpty()) {
+                int findex = index;
+                SwingUtilities.invokeLater(() -> {
+                    frame.setLabel(findex, text.get(0));
+                });
+            }
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Error reading text file", ex);
+        }
+    }
+
+    private void handleJSONFile(String fileName, Path fullPath) {
+        boolean isHorizontal = fileName.contains("horiz");
+        try {
+            Map<String, List<Integer>> rois = parser.parseROI(Files.readAllLines(fullPath));
+            rois.forEach((t, u) -> {
+                int index = Integer.parseInt(t);
+                SwingUtilities.invokeLater(() -> {
+                    frame.setROI(isHorizontal, index, u);
+                });
+            });
+        } catch (IOException | ScriptException ex) {
+            LOG.log(Level.SEVERE, "Error reading json file", ex);
         }
     }
 
